5543294a56758886e859af143bafac1810fe77e1
Marcelo Soares <marcelotc1998@gmail.com>
2022-04-02 10:47:59 -0300

    feat: contador funcional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import classes from './contador.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actionsContador from './store/actions/contador';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const ativarContadorHandler = () => {
+    dispatch(actionsContador.ativarContador());
+  };
+
+  const adicionarContadorHandler = () => {
+    dispatch(actionsContador.adicionarContador());
+  };
+
+  const subtrairContadorHandler = () => {
+    dispatch(actionsContador.subtrairContador());
+  };
+
+  const stateContador = useSelector((state) => {
+    return state;
+  });
+
+  useEffect(() => {
+    console.log(stateContador);
+  }, [stateContador]);
+
   return (
     <main className={classes.contador}>
       <h1>Contador</h1>
       <div>
-        <p className={classes.valor}>0</p>
-        <p>Ative o contador</p>
+        {stateContador.ativado ? (
+          <p className={classes.valor}>{stateContador.contador}</p>
+        ) : (
+          <p>Ative o contador</p>
+        )}
       </div>
       <div>
-        <button className={classes.botao}>+</button>
-        <button>-</button>
-        <button>Ativar</button>
+        {stateContador.ativado ? (
+          <div>
+            <button onClick={adicionarContadorHandler}>+</button>
+            <button onClick={subtrairContadorHandler}>-</button>
+          </div>
+        ) : (
+          <button onClick={ativarContadorHandler}>Ativar</button>
+        )}
       </div>
     </main>
   );
